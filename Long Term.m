e6d3f4e5cf1edeb62251d6ca612e0818b990da93
rpaul93 <paulrishav1@gmail.com>
2015-03-18 00:09:43 +0530

    Update Long Term.m
    Weekday errors added

--- Long Term.m
+++ Long Term.m
@@ -49,6 +49,10 @@ holidays = num(1:end);
 holidays = x2mdate(holidays, 0);
 dayOfWeek = weekday(dateNum);
 
+
+
+
+
 %dayOfWeek(1:5)
 % Non-business days
 isWorkingDay = ~ismember(dateNum,holidays) & ~ismember(dayOfWeek,[1 7]);
@@ -56,8 +60,6 @@ isWorkingDay = ~ismember(dateNum,holidays) & ~ismember(dayOfWeek,[1 7]);
 clear holidays num text data;
 
 X = [daily5dayTempAvg daily5dayDewAvg daily5dayHighAvg dayOfWeek isWorkingDay tempArray dewPntArray ];
-
-
 
 trainInd = dateNum < datenum('2008-01-01');
 
@@ -67,35 +69,113 @@ trainX = X(trainInd,:);
 % same goes for trainy
 trainY = dailyPeakLoad(trainInd);
 
+
+beta = mvregress(trainX, trainY);
+
 testInd = dateNum >= datenum('2008-01-01');
 testX = X(testInd,:);
 testY = dailyPeakLoad(testInd);
 testDates = dateNum(testInd);
 
-disp('hello');
-reTrain = true;
+reTrain = false;
 if reTrain || ~exist('Models\NNModel.mat', 'file')
-    net = newfit(trainX', trainY', 20);
+    net = newfit(trainX', trainY', 100);
     net.performFcn = 'mae';
-    net = train(net, trainX', trainY');
+    net = trainlm(net, trainX', trainY');
     save Models\NNModel.mat net
 else
     load Models\NNModel.mat
 end
 
 forecastLoad = sim(net, testX')';
-
+forecastLoad2 = testX*beta;
 err = testY-forecastLoad;
-fitPlot(testDates, [testY forecastLoad], err);
+err2 = testY - forecastLoad2;
 
+
+%% Error by WeekDay
+
+% Sunday Error
+
+findSun = (dayOfWeek == 1);
+SunTest = testY(findSun(1462:2192,:),:);
+SunForecast = forecastLoad(findSun(1462:2192,:),:);
+
+errSun = SunTest - SunForecast;
+errpctSun = abs(errSun)./SunTest*100;
+MAPESun = mean(errpctSun(~isinf(errpctSun)));
+
+% Monday Error
+
+findMon = (dayOfWeek == 2);
+MonTest = testY(findMon(1462:2192,:),:);
+MonForecast = forecastLoad(findMon(1462:2192,:),:);
+
+errMon = MonTest - MonForecast;
+errpctMon = abs(errMon)./MonTest*100;
+MAPEMon = mean(errpctMon(~isinf(errpctMon)));
+
+% Tuesday Error
+
+findTue = (dayOfWeek == 3);
+TueTest = testY(findTue(1462:2192,:),:);
+TueForecast = forecastLoad(findTue(1462:2192,:),:);
+
+errTue = TueTest - TueForecast;
+errpctTue = abs(errTue)./TueTest*100;
+MAPETue = mean(errpctTue(~isinf(errpctTue)));
+
+
+% Wednesdayday Error
+
+findWed = (dayOfWeek == 4);
+WedTest = testY(findWed(1462:2192,:),:);
+WedForecast = forecastLoad(findWed(1462:2192,:),:);
+
+errWed = WedTest - WedForecast;
+errpctWed = abs(errWed)./WedTest*100;
+MAPEWed = mean(errpctWed(~isinf(errpctWed)));
+
+% Thursday Error
+
+
+findThur = (dayOfWeek == 5);
+thurTest = testY(findThur(1462:2192,:),:);
+thurForecast = forecastLoad(findThur(1462:2192,:),:);
+
+errThur = thurTest - thurForecast;
+errpctThur = abs(errThur)./thurTest*100;
+MAPEThur = mean(errpctThur(~isinf(errpctThur)));
+
+% Friday Error
+
+findFri = (dayOfWeek == 6);
+FriTest = testY(findFri(1462:2192,:),:);
+FriForecast = forecastLoad(findFri(1462:2192,:),:);
+
+errFri = FriTest - FriForecast;
+errpctFri = abs(errFri)./FriTest*100;
+MAPEFri = mean(errpctFri(~isinf(errpctFri)));
+
+% Saturday Error
+
+findSat = (dayOfWeek == 7);
+SatTest = testY(findSat(1462:2192,:),:);
+SatForecast = forecastLoad(findSat(1462:2192,:),:);
+
+errSat = SatTest - SatForecast;
+errpctSat = abs(errSat)./SatTest*100;
+MAPESat = mean(errpctSat(~isinf(errpctSat)));
+
+
+fitPlot(testDates, [testY forecastLoad], err);
+fitPlot(testDates, [testY forecastLoad2], err2);
 
 errpct = abs(err)./testY*100;
-
+errpct2 = abs(err2)./testY*100;
 MAE = mean(abs(err));
 MAPE = mean(errpct(~isinf(errpct)));
+MAPE2 = mean(errpct2(~isinf(errpct2)))
 
 fprintf('Mean Absolute Percent Error (MAPE): %0.2f%% \nMean Absolute Error (MAE): %0.2f MWh\nDaily Peak MAPE: %0.2f%%\n',...
-    MAPE, MAE, mean(errpct))
-
-testY(1:10)
-forecastLoad(1:10)
+    MAPE, MAE, mean(errpct));
